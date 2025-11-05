5caadf2b55f46d1977356f05340c10e1a3a391c3
tankchoi <tadzltv22082004@gmail.com>
2025-11-06 02:25:48 +0700

    #11 - chore: initialize Spring Boot backend project

---
+++ backend/src/main/java/org/opensource/smartair/SmartairApplication.java
@@ -0,0 +1,13 @@
+package org.opensource.smartair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartairApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartairApplication.class, args);
+	}
+
+}
