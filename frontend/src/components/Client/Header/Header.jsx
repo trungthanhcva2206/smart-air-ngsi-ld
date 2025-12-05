6ead84e00766b1f80e46149f065648e4e5b563fc
tankchoi <tadzltv22082004@gmail.com>
2025-12-05 22:23:32 +0700

    chore: Update static pages

--- frontend/src/components/Client/Header/Header.jsx
+++ frontend/src/components/Client/Header/Header.jsx
@@ -42,7 +42,7 @@ const Header = () => {
         <header className="header navbar navbar-expand-lg bg-body-tertiary border-bottom sticky-top">
             <div className="container-fluid">
                 <NavLink to="/" className="navbar-brand fw-bold" style={{ letterSpacing: 0.5 }}>
-                    SmartCity
+                    AirTrack
                 </NavLink>
                 <button
                     className="navbar-toggler"
@@ -60,8 +60,8 @@ const Header = () => {
                         <NavLink to="/" className={({ isActive }) => isActive || window.location.pathname.startsWith('/urn:') ? 'nav-link active' : 'nav-link'}>Bản đồ trạm</NavLink>
                         <NavLink to="/map" className="nav-link">Đường đi</NavLink>
                         <NavLink to="/analysis" className="nav-link">Thống kê</NavLink>
-                        <NavLink to="/air-quality" className="nav-link">Chất lượng không khí</NavLink>
                         <NavLink to="/open-data" className="nav-link">Dữ liệu mở</NavLink>
+                        <NavLink to="/air-quality" className="nav-link">Chất lượng không khí</NavLink>
                         <NavLink to="/about" className="nav-link">Về chúng tôi</NavLink>
                     </nav>
                     <div className="auth-links ms-0 ms-lg-4 d-flex gap-3 align-items-center">
